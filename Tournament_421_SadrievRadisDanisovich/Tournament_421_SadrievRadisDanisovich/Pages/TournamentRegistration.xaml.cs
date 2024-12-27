5468b0bb219008ca4bfa5f16ad5dccd2e90dabf2
p4vuchok <p1vuchok@mail.ru>
2024-12-27 10:26:10 +0300

    Tournaments list page was maid

---
+++ Tournament_421_SadrievRadisDanisovich/Tournament_421_SadrievRadisDanisovich/Pages/TournamentRegistration.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tournament_421_SadrievRadisDanisovich.Components;
+
+namespace Tournament_421_SadrievRadisDanisovich.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для TournamentRegistration.xaml
+    /// </summary>
+    public partial class TournamentRegistration : Page
+    {
+        public List<Tournament> tournament;
+        public TournamentRegistration()
+        {
+            InitializeComponent();
+            tournament = App.db.Tournament.ToList();
+            TournamentList.ItemsSource = tournament;
+        }
+
+        private void AddTour_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
